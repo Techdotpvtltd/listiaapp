8d60b23298d246d022cb68476d882680a40e7d89
Ali Akbar <akbarstudyinfo786@gmail.com>
2024-05-27 16:34:42 +0500

    Category changed feature added

--- lib/screens/main/components/item_type_list.dart
+++ lib/screens/main/components/item_type_list.dart
@@ -6,10 +6,13 @@
 // Description:
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:listi_shop/screens/components/custom_ink_well.dart';
 import 'package:listi_shop/utils/constants/app_theme.dart';
 
+import '../../../blocs/item/item_bloc.dart';
+import '../../../blocs/item/item_state.dart';
 import '../../../models/category_model.dart';
 import '../../../repos/category_repo.dart';
 
@@ -25,7 +28,7 @@ class CategoryListView extends StatefulWidget {
 
 class _CategoryListViewState extends State<CategoryListView> {
   int selectedIndex = 0;
-  late final List<CategoryModel> categories;
+  late List<CategoryModel> categories;
 
   @override
   void initState() {
@@ -40,44 +43,59 @@ class _CategoryListViewState extends State<CategoryListView> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: categories.length,
-      scrollDirection: Axis.horizontal,
-      itemBuilder: (context, index) {
-        return CustomInkWell(
-          onTap: () {
-            setState(() {
-              selectedIndex = index;
-            });
-            widget.onSelectedCategory(categories[selectedIndex]);
-          },
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 5),
-            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 3),
-            decoration: BoxDecoration(
-              gradient: selectedIndex == index
-                  ? AppTheme.primaryLinearGradient
-                  : null,
-              border: selectedIndex == index
-                  ? null
-                  : Border.all(color: Colors.black.withOpacity(0.09)),
-              borderRadius: const BorderRadius.all(Radius.circular(27)),
-            ),
-            child: Center(
-              child: Text(
-                categories[index].item,
-                style: GoogleFonts.plusJakartaSans(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w700,
-                  color: selectedIndex == index
-                      ? Colors.white
-                      : AppTheme.subTitleColor2.withOpacity(0.7),
+    return BlocListener<ItemBloc, ItemState>(
+      listener: (context, state) {
+        if (state is ItemStateAdded || state is ItemStateUpdated) {
+          categories =
+              CategoryRepo().getCategoriesFrom(categoryIds: widget.categories);
+          categories.insert(
+              0,
+              CategoryModel(
+                  id: "all",
+                  item: "All",
+                  createdAt: DateTime.now(),
+                  createdBy: ""));
+        }
+      },
+      child: ListView.builder(
+        itemCount: categories.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return CustomInkWell(
+            onTap: () {
+              setState(() {
+                selectedIndex = index;
+              });
+              widget.onSelectedCategory(categories[selectedIndex]);
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 3),
+              decoration: BoxDecoration(
+                gradient: selectedIndex == index
+                    ? AppTheme.primaryLinearGradient
+                    : null,
+                border: selectedIndex == index
+                    ? null
+                    : Border.all(color: Colors.black.withOpacity(0.09)),
+                borderRadius: const BorderRadius.all(Radius.circular(27)),
+              ),
+              child: Center(
+                child: Text(
+                  categories[index].item,
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                    color: selectedIndex == index
+                        ? Colors.white
+                        : AppTheme.subTitleColor2.withOpacity(0.7),
+                  ),
                 ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
